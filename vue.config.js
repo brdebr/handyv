37078e08e528538919ae6a31cf9f6fdc839d2dae
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2021-10-13 21:04:03 +0200

    Add electron plugin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   lintOnSave: false,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          target: "portable",
+        },
+      },
+    },
+  }
 };
