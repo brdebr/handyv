5af40c0d8b4d76f4a1a6858986a01da2be4c9857
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2021-10-19 00:21:21 +0200

    Remove title bar

--- src/background.ts
+++ src/background.ts
@@ -27,6 +27,8 @@ async function createWindow() {
     },
   });
 
+  if (!isDevelopment) win.setMenu(null);
+
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
